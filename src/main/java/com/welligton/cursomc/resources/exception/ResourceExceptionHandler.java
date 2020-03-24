c0639cd513f6ae7b57a66cc51f6dd1858559884c
Welligton Lins <welligtonlinsufac@gmail.com>
2020-03-24 15:13:11 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/welligton/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/welligton/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.welligton.cursomc.services.exceptions.DataIntegrityException;
 import com.welligton.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,5 +18,12 @@ public class ResourceExceptionHandler {
 		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 	
 	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandarError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+	
+	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/welligton/cursomc/services/CategoriaService.java
+++ src/main/java/com/welligton/cursomc/services/CategoriaService.java
@@ -3,12 +3,12 @@ package com.welligton.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.welligton.cursomc.domain.Categoria;
 import com.welligton.cursomc.repositories.CategoriaRepository;
-
-import javassist.tools.rmi.ObjectNotFoundException;
+import com.welligton.cursomc.services.exceptions.DataIntegrityException;
 
 @Service
 public class CategoriaService {
@@ -30,5 +30,17 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir categoria que possui produtos !");
+		}
+		
+		
+		
+		}
 
 }
