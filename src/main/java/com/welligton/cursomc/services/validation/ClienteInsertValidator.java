416941dc721413312d79e69a3498a9c7080d6f99
Welligton Lins <welligtonlinsufac@gmail.com>
2020-04-30 16:45:37 -0300

    Validação custumizada: email não repetido na atualização de Cliente

--- src/main/java/com/welligton/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/welligton/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.welligton.cursomc.domain.Cliente;
 import com.welligton.cursomc.domain.enums.TipoCliente;
 import com.welligton.cursomc.dto.ClienteNewDTO;
+import com.welligton.cursomc.repositories.ClienteRepository;
 import com.welligton.cursomc.resources.exception.FieldMessage;
 import com.welligton.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,7 +36,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
-		// inclua os testes aqui, inserindo erros na lista
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente !"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
