1686b49d54b424229ba8bba092f24cb594d921b5
Welligton Lins <welligtonlinsufac@gmail.com>
2020-04-30 03:03:01 -0300

    Correcões: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/welligton/cursomc/services/CategoriaService.java
+++ src/main/java/com/welligton/cursomc/services/CategoriaService.java
@@ -31,9 +31,11 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -56,6 +58,8 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
-	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
 	
 }
