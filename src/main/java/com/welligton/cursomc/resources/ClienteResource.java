bbf5d45b4328facb67759dc049806f26df5dc0e1
Welligton Lins <welligtonlinsufac@gmail.com>
2020-04-30 13:10:42 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/welligton/cursomc/resources/ClienteResource.java
+++ src/main/java/com/welligton/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.welligton.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.welligton.cursomc.domain.Cliente;
 import com.welligton.cursomc.dto.ClienteDTO;
+import com.welligton.cursomc.dto.ClienteNewDTO;
 import com.welligton.cursomc.services.ClienteService;
 
 @RestController
@@ -30,9 +33,17 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
 		Cliente obj = service.fromDTO(objDto);
--- src/main/java/com/welligton/cursomc/services/ClienteService.java
+++ src/main/java/com/welligton/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.welligton.cursomc.domain.Cidade;
 import com.welligton.cursomc.domain.Cliente;
+import com.welligton.cursomc.domain.Endereco;
+import com.welligton.cursomc.domain.enums.TipoCliente;
 import com.welligton.cursomc.dto.ClienteDTO;
+import com.welligton.cursomc.dto.ClienteNewDTO;
 import com.welligton.cursomc.repositories.ClienteRepository;
+import com.welligton.cursomc.repositories.EnderecoRepository;
 import com.welligton.cursomc.services.exceptions.DataIntegrityException;
 
 @Service
@@ -20,10 +26,20 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new  com.welligton.cursomc.services.exceptions.ObjectNotFoundException("Objeto nao encontrado ! Id: "+
 				id + ", Tipo: "+ Cliente.class.getName()));
+	}
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return repo.save(obj);
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -51,6 +67,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
