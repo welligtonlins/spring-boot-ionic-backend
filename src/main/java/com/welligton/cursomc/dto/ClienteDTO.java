bc719cb1b38d8001b10a42765349782c296f213d
Welligton Lins <welligtonlinsufac@gmail.com>
2020-05-02 00:37:28 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/welligton/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/welligton/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.welligton.cursomc.domain.Cliente;
+import com.welligton.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
