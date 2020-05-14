6f015e937a282e6f23846517b70e811091f051a9
Welligton Lins <welligtonlinsufac@gmail.com>
2020-05-14 14:55:08 -0300

    Email HTML

--- src/main/java/com/welligton/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/welligton/cursomc/services/SmtpEmailService.java
@@ -1,16 +1,21 @@
 package com.welligton.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	
@@ -20,6 +25,13 @@ public class SmtpEmailService extends AbstractEmailService {
 		mailSender.send(msg);
 		LOG.info("Email enviado");
 		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando HTML...");
+		javaMailSender.send(msg);
+		LOG.info("HTML enviado");
 	}
 
 }
