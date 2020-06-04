9c786fef14cd758dd5a5efdfcc91c59c9b6930c2
Welligton Lins <welligtonlinsufac@gmail.com>
2020-06-04 14:36:23 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/com/welligton/cursomc/domain/Cliente.java
+++ src/main/java/com/welligton/cursomc/domain/Cliente.java
@@ -53,6 +53,7 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
+	private String imageUrl;
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -173,8 +174,12 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
-	
+	public String getImageUrl() {
+		return imageUrl;
+	}
 
-	
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 	
 }
--- src/main/java/com/welligton/cursomc/services/ClienteService.java
+++ src/main/java/com/welligton/cursomc/services/ClienteService.java
@@ -107,7 +107,18 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());
 	}
 	
-	public URI uploadProfilePicture(MultipartFile multpartFile) {
-		return s3Service.uploadFile(multpartFile);
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		URI uri = s3Service.uploadFile(multipartFile);
+
+		Cliente cli = find(user.getId());
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+
+		return uri;
 	}
 }
