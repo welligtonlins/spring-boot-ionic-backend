7d7fa2f7eee7b2ff226f1ae7007baa5558cd81a7
Welligton Lins <welligtonlinsufac@gmail.com>
2020-06-04 21:14:28 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/welligton/cursomc/domain/Cliente.java
+++ src/main/java/com/welligton/cursomc/domain/Cliente.java
@@ -53,7 +53,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -174,12 +173,5 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
 	
 }
